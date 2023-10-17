0bf313406e8f2c30089bbafb9fe850eae0d9b39a
Liân <42004259+laincs@users.noreply.github.com>
2023-10-17 20:33:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pyxel
 
-
-
 class Ball():
     def __init__(self):
         self.x = 80
@@ -14,17 +12,37 @@ class Ball():
         pyxel.circ(self.x,self.y,self.r,7)
         self.x += self.dirX
         self.y += self.dirY
+        self.colliders()
+        
+    def colliders(self):
+        if(self.x <0 or self.x > 160):
+            self.x = 80
+            self.y = 60
+            self.dirX = 1
+            
+        if(self.y < 0 or self.y > 120):
+            self.dirY = -self.dirY 
+            
+        for c in range(0,2):
+            if (self.x > players[c].x-players[c].w and self.x < players[c].x+players[c].w):
+                if (self.y > players[c].y-players[c].h and self.y < players[c].y+players[c].h ):
+                    self.dirX = -self.dirX 
+                    self.dirX+=(1 * (self.dirX>0))
+            
+        
 
 class Player():
     def __init__( self, x):
         self.x = x
         self.y = 80
         self.speed = 2
+        self.w = 2
+        self.h = 15
         
     def draw(self):
-        pyxel.rect(self.x, self.y, 2, 10, 7)
+        pyxel.rect(self.x, self.y, self.w, self.h, 7)
 
-class App:
+class App():
     def __init__(self):
         players.append( Player(20))
         players.append( Player(160 - 20))
@@ -52,4 +70,4 @@ class App:
         
 players=[]
 ball = Ball()
-App()
+app = App()

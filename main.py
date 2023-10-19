998db821f69808b750a224742c24d886ac0d6ed1
Liân <42004259+laincs@users.noreply.github.com>
2023-10-19 19:44:33 -0300

    Arkanoid Update

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import pyxel
+import pyxel, random
+
+AppConfig = {
+  "width": 160,
+  "height": 120
+}
 
 class Ball():
     def __init__(self):
-        self.x = 80
-        self.y = 60
+        self.x = AppConfig["width"]/2
+        self.y = AppConfig["height"]/2
         self.r = 1
-        self.dirX = -1
-        self.dirY = -1
+        self.dirX = ((random.randint(0, 1)*2)-1)*2
+        self.dirY = (-1)*2
         
     def draw(self):
         pyxel.circ(self.x,self.y,self.r,7)
@@ -15,52 +20,48 @@ class Ball():
         self.colliders()
         
     def colliders(self):
-        if(self.x <0 or self.x > 160):
-            self.x = 80
-            self.y = 60
-            self.dirX = 1
+        if(self.x <0 or self.x > AppConfig["width"]):
+            self.dirX = -self.dirX
+            """ 
+            self.x = AppConfig["width"]/2
+            self.y = AppConfig["height"]/2
+            self.dirX = (random.randint(0, 1)*2)-1
+            self.dirY = (random.randint(0, 1)*2)-1 """
             
-        if(self.y < 0 or self.y > 120):
+        if(self.y < 0 ):
             self.dirY = -self.dirY 
+        
+        if(self.y > AppConfig["height"]):
+            print("ded")
             
-        for c in range(0,2):
+        for c in range(0,1):
             if (self.x > players[c].x-players[c].w and self.x < players[c].x+players[c].w):
                 if (self.y > players[c].y-players[c].h and self.y < players[c].y+players[c].h ):
-                    self.dirX = -self.dirX 
-                    self.dirX+=(1 * (self.dirX>0))
+                    self.dirY = -self.dirY 
             
-        
-
 class Player():
     def __init__( self, x):
         self.x = x
-        self.y = 80
+        self.y = AppConfig["height"] - 20
         self.speed = 2
-        self.w = 2
-        self.h = 15
+        self.w = 15
+        self.h = 2
         
     def draw(self):
         pyxel.rect(self.x, self.y, self.w, self.h, 7)
 
 class App():
     def __init__(self):
-        players.append( Player(20))
-        players.append( Player(160 - 20))
-        pyxel.init(160, 120)
-        self.x = 0
+        players.append( Player(AppConfig["width"]/2))
+        pyxel.init(AppConfig["width"], AppConfig["height"])
         pyxel.run(self.update, self.draw)
         
 
     def update(self):
-        if (pyxel.btn(pyxel.KEY_W)):
-            players[0].y-=players[0].speed
-        if (pyxel.btn(pyxel.KEY_S)):
-            players[0].y+=players[0].speed
-            
-        if (pyxel.btn(pyxel.KEY_UP)):
-            players[1].y-=players[1].speed
-        if (pyxel.btn(pyxel.KEY_DOWN)):
-            players[1].y+=players[1].speed
+        if (pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.KEY_LEFT)):
+            players[0].x-=players[0].speed
+        if (pyxel.btn(pyxel.KEY_D) or pyxel.btn(pyxel.KEY_RIGHT)):
+            players[0].x+=players[0].speed
 
     def draw(self):
         pyxel.cls(0)

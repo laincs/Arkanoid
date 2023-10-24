0ae931920f71caabb6d8686ec9ef2e372a072318
Liân <42004259+laincs@users.noreply.github.com>
2023-10-24 20:55:24 -0300

    fun

---
+++ levels.py
@@ -0,0 +1,8 @@
+class Level():
+    def __init__(self, lvlData):
+        self.lvlData = lvlData
+        
+level1 = Level(["bcdefghijklbc",
+                "ghijklbcdefgh",
+                "bcdefghijklbc",
+                "ghijklbcdefgh"])
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pyxel, random
+import levels
 
 AppConfig = {
   "width": 160,
   "height": 120
 }
+
+ColorChar = { "a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7, "i":8, "j":9, "k":10, "l":11 }
 
 class Math():
     def clamp(n, min, max): 
@@ -18,24 +21,31 @@ class Block():
     def __init__(self, ax, ay, color):
         self.w = 10
         self.h = 4
-        self.x = 50
-        self.y = 50
+        self.x = 3 + (ax * (self.w+2))
+        self.y = 3 + (ay * (self.h+2))
         self.color = color
         
     def draw(self):
-        pyxel.rect(self.x, self.y, self.w, self.h, self.color)
+        pyxel.rect(self.x, self.y, self.w, self.h, ColorChar.get(self.color))
 
 class Ball():
-    def __init__(self):
-        self.pinned = True
-        self.x = -100
-        self.y = -100
+    def __init__(self, pinned, color):
+        global players
+        self.pinned = pinned
+        self.color = color
+        self.active = True
+        if(pinned):
+            self.x = -100
+            self.y = -100
+        else:
+            self.x = players[0].x + players[0].w/2 - 1
+            self.y = players[0].y - players[0].h
         self.r = 1
-        self.dirX = ((random.randint(0, 1)*2)-1)*2
+        self.dirX = ((random.randint(0, 1)*2)-1)*(random.random()*2-1)
         self.dirY = (-1)*2
         
     def draw(self):
-        pyxel.circ(self.x,self.y,self.r,7)
+        pyxel.circ(self.x,self.y,self.r,self.color) #7 white
         if(self.pinned):
             self.x = players[0].x + players[0].w/2 - 1
             self.y = players[0].y - players[0].h
@@ -55,13 +65,24 @@ class Ball():
             self.dirY = -self.dirY 
         
         if(self.y > AppConfig["height"]):
+            global balls
+            balls.remove(self)
             App.ballDestroy()
             
         for c in range(0,1):
-            if (self.x > players[c].x-players[c].w and self.x < players[c].x+players[c].w):
+            if (self.x > players[c].x and self.x < players[c].x+players[c].w):
                 if (self.y > players[c].y-players[c].h and self.y < players[c].y+players[c].h ):
                     self.dirX = self.getDir()
                     self.dirY = -( self.dirY )
+        
+            for b in blocks:
+                if(self.x > b.x and self.x < b.x+b.w):
+                    if(self.y > b.y and self.y < b.y + b.h):
+                        if(self.x < b.x+1 or self.x > b.x+b.w-1):
+                            self.dirX = -( self.dirX )
+                        else:
+                            self.dirY = -( self.dirY )
+                        blocks.remove(b)
                     
     def getDir(self):
         value = (((Math.clamp(players[0].x - self.x,-players[0].w,0)) + (players[0].w/2))/(-players[0].w/2))*2
@@ -81,19 +102,40 @@ class Player():
 
 class App():
     def __init__(self):
-        blocks.append( Block(50,50, 12) )
-        players.append( Player(AppConfig["width"]/2))
+        self.awake()
         pyxel.init(AppConfig["width"], AppConfig["height"])
         pyxel.run(self.update, self.draw)
         
+    def awake(self):
+        players.append( Player(AppConfig["width"]/2))
+        for c in range(0,10):
+            balls.append( Ball(False, random.randint(1,12)))
+        self.buildLvl(levels.level1)
+    
+    def buildLvl(self, lvl):
+        print(lvl.lvlData[0][0])
+        for y in range (0, len(lvl.lvlData)):
+            ax = 0
+            for c in lvl.lvlData[y]:
+                if(c != "_"): blocks.append( Block(ax, y, c) )
+                ax+=1
 
     def update(self):
+        autoPlay = True
+        offset = 3
+        if(autoPlay):
+            if(balls[0].x > players[0].x + (players[0].w/2) -offset):
+                players[0].x+=2
+            if(balls[0].x < players[0].x + (players[0].w/2) +offset):
+                players[0].x-=2
+        
+        
         if (pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.KEY_LEFT)):
             players[0].x-=players[0].speed
         if (pyxel.btn(pyxel.KEY_D) or pyxel.btn(pyxel.KEY_RIGHT)):
             players[0].x+=players[0].speed
-        if (pyxel.btn(pyxel.KEY_SPACE) or pyxel.btn(pyxel.KEY_UP)):
-            ball.throwBall()
+        """ if (pyxel.btn(pyxel.KEY_SPACE) or pyxel.btn(pyxel.KEY_UP)):
+            ball.throwBall() """
 
     def draw(self):
         pyxel.cls(0)
@@ -101,13 +143,17 @@ class App():
             c.draw()
         for c in blocks:
             c.draw()
-        ball.draw()
+        for c in balls:
+            c.draw()
         
     def ballDestroy():
-        global ball
-        ball = Ball()
+        global balls
+        for c in range(0,10):
+            if(len(balls)<2000):
+                print(len(balls))
+                balls.append( Ball(False, random.randint(1,12)))
         
 blocks=[]
 players=[]
-ball = Ball()
+balls = []
 app = App()
